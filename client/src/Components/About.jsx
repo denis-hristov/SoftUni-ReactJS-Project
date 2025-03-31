2586400aee2e37a1f1994d45984e4a03b3547ba0
Denis Hristov <denis.hristov145@gmail.com>
2025-03-31 12:20:59 +0300

    Fixed About

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -15,18 +15,21 @@ export default function About() {
                 </div>
             </div>
             <div className="about-section">
-                <p>
-                    At Vivid Chocolatrie, we specialize in creating exquisite chocolates, chocolate-covered strawberries, truffles, and éclairs - perfect for any special occasion or simply as an indulgent treat for your everyday moments. Our journey to perfecting our signature chocolate recipes has been filled with dedication and experimentation. We have spent countless hours perfecting each flavor, each texture, until we could create the irresistible chocolates you enjoy today. It has not always been easy, but the hard work has been worth it!
-                </p>
-                <div className="images-container">
-                    <img src="/photos/Making/Making1.jpg" alt="Chocolate-making process 1" className="about-image"/>
+                <div className="part1">
+                    <p>
+                        At Vivid Chocolatrie, we specialize in creating exquisite chocolates, chocolate-covered strawberries, truffles, and éclairs - perfect for any special occasion or simply as an indulgent treat for your everyday moments. Our journey to perfecting our signature chocolate recipes has been filled with dedication and experimentation. We have spent countless hours perfecting each flavor, each texture, until we could create the irresistible chocolates you enjoy today. It has not always been easy, but the hard work has been worth it!
+                    </p>
+                    <div className="images-container">
+                        <img src="/photos/Making/Making2.jpg" alt="Chocolate-making process 1" className="about-image"/>
+                    </div>
                 </div>
-                <p>
-                    Our mission is to offer you more than just chocolates - we want to offer you a moment of pure joy, wrapped in a cute box. Whether it's a gift for a loved one or a personal indulgence, we believe our treats will add a touch of sweetness to every moment.
-                </p>
-                
-                <div className="images-container">
-                    <img src="/photos/Making/Making2.jpg" alt="Chocolate-making process 1" className="about-image"/>
+                <div className="part2">
+                    <p>
+                        Our mission is to offer you more than just chocolates - we want to offer you a moment of pure joy, wrapped in a cute box. Whether it's a gift for a loved one or a personal indulgence, we believe our treats will add a touch of sweetness to every moment.
+                    </p>
+                    <div className="images-container">
+                        <img src="/photos/Making/Making1.jpg" alt="Chocolate-making process 1" className="about-image"/>
+                    </div>
                 </div>
             </div>
         </>
